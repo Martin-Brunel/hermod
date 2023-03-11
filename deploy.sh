2e52907d13d37c6c991952e0422aff9b8f2d2baa
martin <martinbrunel@hotmail.fr>
2023-03-11 21:53:11 +0100

    save

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 git pull;
 sudo systemctl stop hermod.service;
-/home/ubuntu/.cargo/bin/diesel migration run;
+diesel migration run;
 cargo build --release;
 sudo systemctl start hermod.service;
 exit 0;
